c6e829fb7fb3bba55ef423fad5e703d9b2ca6dbf
Andrew Maney <amaney@usc.edu>
2018-03-05 05:50:31 -0800

    some old changes

--- public/javascripts/socket-client.js
+++ public/javascripts/socket-client.js
@@ -4,16 +4,16 @@ const io = require('socket.io-client');
 var socket = io();
 socket.on('new donation', newDonation);
 
+const shotsAudio = document.getElementById('shots');
 function newDonation(body) {
   var donations = $('.donations');
   donations.find('#newest').removeAttr('id');
   
+  shotsAudio.play();
   const donationElem = `
     <div class="donation" id="newest">
-      <div class="profile">
-        <img class="picture" src="${body.picture}") />
-        <p class="name">${body.name}</p>
-      </div>
+      <img class="picture" src="${body.picture}") />
+      <p class="name">${body.name}</p>
       <p class="amount">${body.amount}</p>
       <p class="memo">${body.memo}</p>
     </div>
