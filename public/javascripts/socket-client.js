9862bc6048ba2af6d959be7ffab1e44f8fa0d661
Andrew Maney <amaney@usc.edu>
2018-03-05 04:18:23 -0800

    there's shit to see now

--- public/javascripts/socket-client.js
+++ public/javascripts/socket-client.js
@@ -1,9 +1,23 @@
+import $ from "jquery";
 const io = require('socket.io-client');
 
 var socket = io();
-socket.on('new donation', function(body) {
-  document.getElementById('picture').src = body.picture;
-  document.getElementById('name').innerHTML = body.name;
-  document.getElementById('amount').innerHTML = body.amount;
-  document.getElementById('memo').innerHTML = body.memo;
-});
+socket.on('new donation', newDonation);
+
+function newDonation(body) {
+  var donations = $('.donations');
+  donations.find('#newest').removeAttr('id');
+  
+  const donationElem = `
+    <div class="donation" id="newest">
+      <div class="profile">
+        <img class="picture" src="${body.picture}") />
+        <p class="name">${body.name}</p>
+      </div>
+      <p class="amount">${body.amount}</p>
+      <p class="memo">${body.memo}</p>
+    </div>
+  `;
+
+  donations.prepend(donationElem);
+}
